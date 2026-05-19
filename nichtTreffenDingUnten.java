f6257bf9e46590011cad7e20096dbfcb4b280981
Julian Mersmann <mersmannjulian@gmail.com>
2026-05-19 17:32:41 +0200

    Update bottom coral to support paired gap positioning

--- nichtTreffenDingUnten.java
+++ nichtTreffenDingUnten.java
@@ -12,13 +12,27 @@ public class nichtTreffenDingUnten
     // Konstruktor
     public nichtTreffenDingUnten(double pX, double pY )
     {
-        
         K2 = new Picture(pX, pY,"Koralle2.png");
     }
-    
-    private void drehen(){
-        
-    
+
+    public void bewege() {
+        K2.move(-2,0);
+    }
+
+    public void setzePosition(double pX, double pY) {
+        K2.moveTo(pX, pY);
+    }
+
+    public Picture getShape() {
+        return K2;
+    }
+
+    public double getShapeX(){
+        return K2.getShapeX();
+    }
+
+    public double getBreite(){
+        return K2.getShapeWidth();
     }
     // Dienste
 
