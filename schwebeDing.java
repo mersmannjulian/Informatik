81bcef353ffdaefeb5dfe2ead65493da17fbc371
Julian Mersmann <mersmannjulian@gmail.com>
2026-05-19 17:32:10 +0200

    Check collisions with top and bottom corals

--- schwebeDing.java
+++ schwebeDing.java
@@ -6,36 +6,39 @@ public class schwebeDing
     // Bezugsobjekte
     private Picture snapperBild;
     private Picture Backfisch;
-    private nichtTreffenDingOben[] dieHindernisse;
+    private nichtTreffenDingOben[] dieHindernisseOben;
+    private nichtTreffenDingUnten[] dieHindernisseUnten;
     // Attribute
     //private boolean aktiv;
     // Konstruktor
-    public  schwebeDing(nichtTreffenDingOben[] pK1)
+    public  schwebeDing(nichtTreffenDingOben[] pOben, nichtTreffenDingUnten[] pUnten)
     {
-        snapperBild = new Picture(500 , 500,"snapper.png");
+        snapperBild = new Picture(Spiel.BILDBREITE - 200 , Spiel.BILDHOEHE / 2,"snapper.png");
         Backfisch = new Picture(0,-100,"explosion.png");
         Backfisch.setHidden(false);
         //aktiv = true;
-        dieHindernisse = pK1;
+        dieHindernisseOben = pOben;
+        dieHindernisseUnten = pUnten;
     }
     
-        public void bewegeOben() {
-        //snapperBild.move(0,-1);
-        if (snapperBild.getShapeX() > 1) {
+    public void bewegeOben() {
+        if (snapperBild.getShapeY() > 1) {
             snapperBild.move(0,-1);
         }
     }
     public void bewegeUnten() {
-        //snapperBild.move(0,1);
-        if (snapperBild.getShapeX() < Spiel.BILDBREITE-snapperBild.getShapeWidth()-1) {
+        if (snapperBild.getShapeY() < Spiel.BILDHOEHE-snapperBild.getShapeHeight()-1) {
             snapperBild.move(0,1);
         }
     }
     
     public boolean pruefeKollision() {
         boolean ergebnis = false;
-        for (int i = 0; i < dieHindernisse.length; i++) {
-            if (snapperBild.intersects(dieHindernisse[i].getShape())) {
+        for (int i = 0; i < dieHindernisseOben.length; i++) {
+            if (snapperBild.intersects(dieHindernisseOben[i].getShape())) {
+                ergebnis = true;
+            }
+            if (snapperBild.intersects(dieHindernisseUnten[i].getShape())) {
                 ergebnis = true;
             }
         }
